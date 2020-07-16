502531f1d44896855469195262c2269346f3a796
d0rich <51794590+d0rich@users.noreply.github.com>
2020-07-16 15:43:23 +0700

    fillDictionary (почти готово)

--- app/routes/fillDictionary.js
+++ app/routes/fillDictionary.js
@@ -1,11 +1,29 @@
 module.exports = function(app, models, jsonParser) {
     app.post('/fillDictionary', jsonParser, async(req, res) => {
-        const Genders = ['m', 'f']
+        const Genders = ['Мужской', 'Женский']
         const Sports = ['1', '2', '3']
         const Events = ['1', '2', '3']
         const Nots = ['a', 'b', 'c']
         Genders.forEach(gender => {
             models.Gender_type.create({ Name: gender })
+                .then(result => {
+                    console.log(result.dataValues);
+                }).catch(err => console.log(err))
+        });
+        Sports.forEach(sport => {
+            models.Sport_type.create({ Name: sport })
+                .then(result => {
+                    console.log(result.dataValues);
+                }).catch(err => console.log(err))
+        });
+        Events.forEach(event => {
+            models.Event_type.create({ Name: event })
+                .then(result => {
+                    console.log(result.dataValues);
+                }).catch(err => console.log(err))
+        });
+        Nots.forEach(not => {
+            models.Not_type.create({ Name: not })
                 .then(result => {
                     console.log(result.dataValues);
                 }).catch(err => console.log(err))
