96564f22c6a65ef0254327529ecd1200d09ecf0d
d0rich <d.orich@yandex.ru>
2020-07-28 11:11:23 +0700

    get user by id

--- app/routes/get/user.js
+++ app/routes/get/user.js
@@ -1,14 +1,18 @@
 module.exports = function(app, models) {
-    app.get('/user', async(req, res) => {
-        const Login = req.param('login')
-        if (!Login) return res.sendStatus(400)
-        models.User.findOne({ where: { Login: Login } })
-            .then(user => {
-                if (user == null) return res.sendStatus(404)
-                res.send(user)
-            })
-            .catch(err => {
-                res.send(err)
-            })
-    })
+  app.get('/user', async(req, res) => {
+    const Login = req.param('login')
+    const ID = req.param('id')
+    if (!Login || !ID) return res.sendStatus(400)
+    let detail = {}
+    if (Login) detail = { where: { Login: Login } }
+    if (ID) detail = { where: { ID: ID } }
+    models.User.findOne(detail)
+      .then(user => {
+        if (user == null) return res.sendStatus(404)
+        res.send(user)
+      })
+      .catch(err => {
+        res.send(err)
+      })
+  })
 }
