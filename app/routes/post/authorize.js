e6e42e0129175ac231a46514f08682b17a5b6da7
d0rich <51794590+d0rich@users.noreply.github.com>
2020-07-28 13:57:15 +0700

    +

--- app/routes/post/authorize.js
+++ app/routes/post/authorize.js
@@ -11,28 +11,31 @@ module.exports = function(app, models, jsonParser) {
         const Login = req.body.login
         const Password = req.body.password
         if (!Login || !Password) return res.sendStatus(400)
-        models.User.findOne({
-                where: {
-                    Login: Login,
-                    Password: Password
-                }
-            })
+        models.User.findOne({ where: { Login: Login } })
             .then(user => {
-                if (user == null) return res.sendStatus(404)
-                console.log('wasted')
-                let header = { alg: "SHA256", typ: "JWT" }
-                let payload = {
-                    userID: user.ID,
-                    created: new Date().toISOString(),
-                    sub: 'access'
-                }
-                let secretKey = 'S4vEHyg69aLL1uLLlk4'
-                let unsignedToken = encodeBase64Url(JSON.stringify(header)) + '.' + encodeBase64Url(JSON.stringify(payload))
-                let signature = crypto.createHmac('sha256', secretKey).update(unsignedToken).digest('hex')
-                let token = encodeBase64Url(JSON.stringify(header)) + '.' + encodeBase64Url(JSON.stringify(payload)) + '.' + encodeBase64Url(signature)
-                res.send(token)
-
-            }).catch(err => {
+                crypto.pbkdf2(Password, user.Salt, 1000, 256, (err, key) => {
+                    if (err) {
+                        console.log(err)
+                        return res.sendStatus(500)
+                    }
+                    if (key.toString('hex') == user.Password) {
+                        if (user == null) return res.sendStatus(404)
+                        console.log('wasted')
+                        let header = { alg: "SHA256", typ: "JWT" }
+                        let payload = {
+                            userID: user.ID,
+                            created: new Date().toISOString(),
+                            sub: 'access'
+                        }
+                        let secretKey = 'S4vEHyg69aLL1uLLlk4'
+                        let unsignedToken = encodeBase64Url(JSON.stringify(header)) + '.' + encodeBase64Url(JSON.stringify(payload))
+                        let signature = crypto.createHmac('sha256', secretKey).update(unsignedToken).digest('hex')
+                        let token = encodeBase64Url(JSON.stringify(header)) + '.' + encodeBase64Url(JSON.stringify(payload)) + '.' + encodeBase64Url(signature)
+                        res.send(token)
+                    } else return res.sendStatus(404)
+                })
+            })
+            .catch(err => {
                 res.send(err)
             })
 
