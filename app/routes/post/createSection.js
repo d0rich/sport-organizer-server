dedfdb0fb1fa1eb05a2f83921c3c55a612427cc3
d0rich <d.orich@yandex.ru>
2020-07-31 14:30:53 +0700

    Коля рукожоп

--- app/routes/post/createSection.js
+++ app/routes/post/createSection.js
@@ -1,18 +1,17 @@
 module.exports = function(app, models, jsonParser) {
-    app.post('/createSection', jsonParser, async(req, res) => {
-        if (!req.body) return res.sendStatus(400);
-        const NewSection = req.body;
-        models.User.findOne({ where: { Login: NewSection.CreatorLogin } }).then(user => {
-            models.Section.create({
-                    Name: NewSection.Name,
-                    Description: NewSection.Description,
-                    CreatorID: user.ID,
-                    SportTypeID: NewSection.Sport_type
-                })
-                .then(result => {
-                    console.log(result.dataValues);
-                    res.send(result.dataValues)
-                }).catch(err => console.error(err))
-        })
-    })
+  app.post('/createSection', jsonParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    const NewSection = req.body;
+    models.Section.create({
+        Name: NewSection.Name,
+        Description: NewSection.Description,
+        CreatorID: NewSection.CreatorID,
+        SportTypeID: NewSection.SportTypeID
+      })
+      .then(result => {
+        console.log(result.dataValues);
+        res.send(result.dataValues)
+      }).catch(err => console.error(err))
+
+  })
 }
