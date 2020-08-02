1dbf5a21d04400c44509078b7b00c934078f6b08
d0rich <51794590+d0rich@users.noreply.github.com>
2020-08-02 18:57:15 +0700

    +eh

--- app/routes/post/updateInf.js
+++ app/routes/post/updateInf.js
@@ -1,8 +1,18 @@
 module.exports = function(app, models, jsonParser) {
     app.post('/updateInf', jsonParser, async(req, res) => {
         if (!req.body) return res.sendStatus(400)
-
-
-
+        const NewInfo = req.body;
+        models.User.update({
+                Name: NewInfo.Name,
+                Surname: NewInfo.Surname,
+                Birthdate: NewInfo.Birthdate,
+                GenderTypeID: NewInfo.Gender,
+                Height: NewInfo.Height,
+                Weight: NewInfo.Weight
+            }, { where: { ID: ID } })
+            .then(result => {
+                console.log(result.dataValues);
+                res.send(result.dataValues)
+            }).catch(err => console.error(err))
     })
 }
