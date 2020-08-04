b54ba2e86c4ab279fe0062f9b6856de76022880e
d0rich <51794590+d0rich@users.noreply.github.com>
2020-08-04 16:29:18 +0700

    +bez_proverki

--- app/routes/post/updateInf.js
+++ app/routes/post/updateInf.js
@@ -1,21 +1,27 @@
 module.exports = function(app, models, jsonParser) {
-    app.post('/updateInf', jsonParser, async(req, res) => {
+    app.post('/updateInf', jsonParser, (req, res) => {
         if (!req.body) return res.sendStatus(400)
         const NewInfo = req.body;
-        let user = await models.User.findOne({ where: { ID: NewInfo.ID } })
-        if (!user) return res.sendStatus(404);
-        models.User.update({
-                Name: NewInfo.Name,
-                Surname: NewInfo.Surname,
-                Birthdate: NewInfo.Birthdate,
-                GenderTypeID: NewInfo.Gender,
-                Height: NewInfo.Height,
-                Weight: NewInfo.Weight
-            }, { where: { ID: user.ID } })
-            .then(result => {
-                console.log(result.dataValues);
-                res.send(result.dataValues)
-            }).catch(err => {
+        models.User.findOne({ where: { ID: NewInfo.ID } })
+            .then(user => {
+                if (!user) return res.sendStatus(404);
+                models.User.update({
+                        Name: NewInfo.Name,
+                        Surname: NewInfo.Surname,
+                        Birthdate: NewInfo.Birthdate,
+                        GenderTypeID: NewInfo.Gender,
+                        Height: NewInfo.Height,
+                        Weight: NewInfo.Weight
+                    }, { where: { ID: user.ID } })
+                    .then(result => {
+                        console.log(result);
+                        res.send(result)
+                    }).catch(err => {
+                        console.error(err)
+                        res.sendStatus(500)
+                    })
+            })
+            .catch(err => {
                 console.error(err)
                 res.sendStatus(500)
             })
