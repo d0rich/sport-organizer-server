ce2db511a31395c132d9670c7b38e621434d57fa
Nikolay Dorofeev <d.orich@yandex.ru>
2020-08-04 18:05:33 +0700

    fix create group

--- app/routes/post/createGroup.js
+++ app/routes/post/createGroup.js
@@ -1,18 +1,16 @@
 module.exports = function(app, models, jsonParser) {
-    app.post('/createGroup', jsonParser, async(req, res) => {
-        if (!req.body) return res.sendStatus(400);
-        const NewGroup = req.body;
-        models.Section.findOne({ where: { Name: NewGroup.SectionName } }).then(section => {
-            models.Group.create({
-                    Name: NewGroup.Name,
-                    Description: NewGroup.Description,
-                    SectionID: section.ID,
-                    AgeTypeID: NewGroup.Age_type
-                })
-                .then(result => {
-                    console.log(result.dataValues);
-                    res.send(result.dataValues)
-                }).catch(err => console.error(err))
-        })
-    })
+  app.post('/createGroup', jsonParser, async(req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    const NewGroup = req.body;
+    models.Group.create({
+        Name: NewGroup.Name,
+        Description: NewGroup.Description,
+        SectionID: NewGroup.SectionID,
+        AgeTypeID: NewGroup.Age_type
+      })
+      .then(result => {
+        console.log(result.dataValues);
+        res.send(result.dataValues)
+      }).catch(err => console.error(err))
+  })
 }
