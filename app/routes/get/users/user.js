65fb707501f4131cd02b6dc72401a824f18510bc
khromachu <nadushok.khrom777@gmail.com>
2020-08-25 17:50:11 +0700

    +user update

--- app/routes/get/users/user.js
+++ app/routes/get/users/user.js
@@ -13,8 +13,20 @@ module.exports = function(app, models) {
                     { model: models.Gender_type, attributes: ['ID', 'Name'] },
                     { model: models.Sport_type, as: 'Sports', attributes: ['ID', 'Name'], through: { attributes: [] } },
                     { model: models.Section, attributes: ['ID', 'Name'] },
-                    { model: models.Group, as: 'TrainerIn', attributes: ['ID', 'Name'], through: { model: models.Trainer, attributes: [] } },
-                    { model: models.Group, as: 'TraineeIn', attributes: ['ID', 'Name'], through: { model: models.Trainee, attributes: [] } }
+                    {
+                        model: models.Group,
+                        as: 'TrainerIn',
+                        attributes: ['ID', 'Name'],
+                        through: { model: models.Trainer, attributes: [] },
+                        include: [{ model: models.Section, attributes: ['ID', 'Name'] }]
+                    },
+                    {
+                        model: models.Group,
+                        as: 'TraineeIn',
+                        attributes: ['ID', 'Name'],
+                        through: { model: models.Trainee, attributes: [] },
+                        include: [{ model: models.Section, attributes: ['ID', 'Name'] }]
+                    }
                 ]
             })
             .then(user => {
