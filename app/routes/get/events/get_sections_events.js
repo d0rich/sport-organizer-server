489abaf3e0fa81622cc5647a8107ffec94cb0c23
khromachu <nadushok.khrom777@gmail.com>
2020-08-27 12:14:03 +0700

    +mini-update

--- app/routes/get/events/get_sections_events.js
+++ app/routes/get/events/get_sections_events.js
@@ -1,22 +1,18 @@
 module.exports = function(app, sequelize) {
     app.get('/events/get/bySections', (req, res) => {
         const ID = req.param('sectionID')
-        const Date = JSON.parse(req.param('date'))
-        let query = `select distinct N."ID", N."Comment", N."createdAt",
-        N."updatedAt", N."NotTypeID", N."EventID", N."UserID",
-        "Not-types"."ID" as "Not-type.ID","Not-types"."Name" as "Not-type.Name",
-        "Users"."ID" as "User.ID","Users"."Login" as "User.Login",
-        "Users"."Name" as "User.Name","Users"."Surname" as "User.Surname",
-        "Events"."ID" as "Event.ID","Events"."Name" as "Event.Name","Events"."TimeRange" as "Event.TimeRange"
+        const StartDate = req.param('date')
+        const EndDate = new Date(StartDate).setMilliseconds(24 * 60 * 60 * 1000).toISOString()
+        let query = `select distinct 
+        "Events"."ID","Events"."Name","Events"."TimeRange",
+        "Event-types"."ID" as "Event-type.ID" ,"Event-types"."Name" as "Event-type.Name"
         from "Events"
+        inner join "Event-types" on "Events"."EventTypeID" = "Event-types"."ID" 
         inner join "Events-Groups" on "Events"."ID" = "Events-Groups"."EventID"
         inner join "Groups" on "Events-Groups"."GroupID" = "Groups"."ID"
         inner join "Sections" on "Groups"."SectionID" = "Sections"."ID"
-        inner join "Notifications" N on "Events"."ID" = N."EventID"
-        inner join  "Users" on N."UserID" = "Users"."ID"
-        inner join "Not-types" on N."NotTypeID" = "Not-types"."ID"
         where "Sections"."ID"='${ID}'
-        and ('${Date}' between date(lower("Events"."TimeRange")) and date(upper("Events"."TimeRange")))`
+        and (lower("Events"."TimeRange") < '${EndDate}') and (upper("Events"."TimeRange")>'${StartDate}')`
         sequelize.query(query, {
                 type: sequelize.QueryTypes.SELECT,
                 nest: true
