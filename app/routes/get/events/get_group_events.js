36f519f954677b08a6ec845aa803df9aa0008088
d0rich <d.orich@yandex.ru>
2020-08-27 14:07:34 +0700

    get groups events + includeArrays

--- app/routes/get/events/get_group_events.js
+++ app/routes/get/events/get_group_events.js
@@ -1,3 +1,4 @@
+const includeArrays = require('../../../functions/includeArrays')
 module.exports = function(app, sequelize) {
     app.get('/events/get/byGroups', (req, res) => {
         const Groups = JSON.parse(req.param('groups'))
@@ -8,7 +9,7 @@ module.exports = function(app, sequelize) {
         "Events"."Name", "Events"."Description", "Events"."TimeRange", "Events"."Timed",
         "Events"."EventTypeID",
         "Event-types"."ID" as "Event-type.ID" ,"Event-types"."Name" as "Event-type.Name",
-        "Groups"."ID" as "Group.ID"
+        "Groups"."ID" as "Groups.ID"
         from "Events"
         inner join "Events-Groups" on "Events"."ID" = "Events-Groups"."EventID"
         inner join "Groups" as "Groups" on "Events-Groups"."GroupID" = "Groups"."ID"
@@ -24,7 +25,7 @@ module.exports = function(app, sequelize) {
                 nest: true
             })
             .then(groups => {
-                res.send(groups)
+                res.send(includeArrays(groups, ['Groups']))
             })
             .catch(err => {
                 console.error(err)
